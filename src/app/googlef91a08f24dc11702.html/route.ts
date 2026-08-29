64298bb61a1b39bad98b114992e1a6586659a1df
Omar Abdel Rahim Mah'd Aljaradat <32402328014@std.bau.edu.jo>
2026-08-29 22:57:00 +0300

    feat(seo): add route handler for google html verification

---
+++ src/app/googlef91a08f24dc11702.html/route.ts
@@ -0,0 +1,7 @@
+﻿import { NextResponse } from 'next/server';
+
+export function GET() {
+  return new NextResponse('google-site-verification: googlef91a08f24dc11702.html', {
+    headers: { 'Content-Type': 'text/html; charset=utf-8' },
+  });
+}
