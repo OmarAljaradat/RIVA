8bbd386bfb9d0483a6d79b4a87922500bd7a1df4
Omar Abdel Rahim Mah'd Aljaradat <32402328014@std.bau.edu.jo>
2026-08-31 19:08:46 +0300

    fix(config): revert serverExternalPackages

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  serverExternalPackages: ['@prisma/adapter-pg', 'pg', 'telegram'],
   // ─── Security Headers ────────────────────────────────────────────────────
   async headers() {
     return [
